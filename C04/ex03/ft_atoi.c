dfade3699c95e331ed06eaac91985a5eb0c95b23
Hyunji Cho <hyucho@c6r1s5.42seoul.kr>
2021-09-27 20:11:08 +0900

    ex03 debug

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: hyucho <hyucho@student.42Seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/23 13:09:55 by hyucho            #+#    #+#             */
-/*   Updated: 2021/09/26 15:58:52 by hyucho           ###   ########.fr       */
+/*   Updated: 2021/09/27 20:10:07 by hyucho           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,38 @@ int	check_num(char num)
 	return (0);
 }
 
+int	check_alpha(char num)
+{
+	if ((num >= 'a' && num <= 'z') || (num >= 'A' && num <= 'Z'))
+		return (1);
+	return (0);
+}
+
+int	check_space(char num)
+{
+	if ((num > 8 && num < 14) && num == ' ')
+		return (1);
+	return (0);
+}
+
 int	ft_atoi(char *str)
 {
 	int	i;
 	int	minus;
 	int	num;
 
-	i = 0;
+	i = -1;
 	num = 0;
 	minus = 1;
+	while (1)
+		if (!(check_space(str[++i])))
+			break ;
 	while (str[i] != '\0')
 	{
 		if (i != 0 && !(check_num(str[i])) && check_num(str[i - 1]))
+			break ;
+		else if ((str[i] == '-' || str[i] == '+') && \
+				(check_alpha(str[i + 1]) || check_space(str[i + 1])))
 			break ;
 		else if (str[i] == '-')
 			minus *= -1;
