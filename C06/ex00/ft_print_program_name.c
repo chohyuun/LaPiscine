74cd1d6d2e38b61377fda4faaae13d947f7bdcdc
Hyunji Cho <hyucho@c6r1s7.42seoul.kr>
2021-09-28 21:48:10 +0900

    ex00 Debug

--- C06/ex00/ft_print_program_name.c
+++ C06/ex00/ft_print_program_name.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyucho <hyucho@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/28 20:03:34 by hyucho            #+#    #+#             */
+/*   Updated: 2021/09/28 21:47:18 by hyucho           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include<unistd.h>
 
 void	ft_print(char *str)
@@ -12,5 +24,6 @@ void	ft_print(char *str)
 
 int	main(int argc, char **argv)
 {
-	ft_print(argv[argc - 1]);
+	(void)argc;
+	ft_print(argv[0]);
 }
